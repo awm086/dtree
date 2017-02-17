c956c3335807a3230d1144fc48fd7b5796d8c954
Ali <aalmusta@its.jnj.com>
2017-02-17 17:35:19 -0500

    remove old dirs

--- assignment4/src/items.controller.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  angular.module('MenuApp')
-    .controller('ItemsController', ItemsController)
-
-  ItemsController.$inject = ['items'];
-  function ItemsController(items) {
-    this.items = items;
-  }
-})();
--- assignment4/src/menuapp.module.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ali on 12/12/16.
- */
-(function () {
-  'use strict';
-
-  angular.module('MenuApp', ['ui.router', 'Data']);
-
-})();
